217765e38f52ff12cc78675d3b102a7d83809369
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-04-25 20:43:53 +0530

    passport configured

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import express from 'express';
 import http from 'http';
 import dotenv from "dotenv"
 
+import passport from 'passport';
+import session from "express-session";
+import connectMongo from "connect-mongodb-session"
+import { buildContext } from "graphql-passport";
+
+
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
@@ -11,14 +17,35 @@ import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHt
 import mergedResolver from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 import { connectDB } from './db/connectDB.js';
+import { configurePassport } from './passport/passport.config.js';
 
 dotenv.config()
+configurePassport()
+
 const app = express()
 app.use(express.urlencoded({ extended: true }));
 
 
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: "sessions",
+})
+
+store.on("error", (err) => console.log(err));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }, store: store
+}))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
@@ -32,12 +59,14 @@ await server.start();
 
 app.use(
     '/',
-    cors(),
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }),
     express.json(),
     expressMiddleware(server, {
-        context: async ({ req }) => {
-            console.log("({ req })", req)
-            return { req }
+        context: async ({ req, res }) => {
+            return buildContext({ req, res })
         },
     }),
 );
