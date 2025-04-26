e986ad7c248d15b4cdb25912dd83b3c436ffe7ff
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-04-26 13:46:05 +0530

    ui design added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignupPage from "./pages/SignupPage"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import TransactionPage from "./pages/TransactionPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import Header from './components/ui/Header'
+
+const App = () => {
+  const authUser = true
+  return (
+    <>
+      {
+        authUser && <Header />
+      }
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignupPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App 
